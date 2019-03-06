88a310b849c6ee97f172be85926af25e26304fca
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2019-03-06 20:12:39 +0530

    adding cors to server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 let port = process.env.PORT || 3000;
 
