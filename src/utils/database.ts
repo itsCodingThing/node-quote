b08e5cb59c5fde74ac432dac4d837bb82aca2a90
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2020-08-06 19:53:34 +0530

    removing comment in database.js

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -1,22 +1,6 @@
 import { initializeApp, credential as _credential, database, firestore, ServiceAccount } from "firebase-admin";
 import { QuoteObj } from "./quotes";
 
-// <([a-z]+)([^<]+)*(?:>(.*)<\/\1>|\s+\/>)
-//  needed admin credential for firebase project
-// const config: ServiceAccount = {
-//   type: "service_account",
-//   project_id: "node-quote-database",
-//   auth_uri: "https://accounts.google.com/o/oauth2/auth",
-//   token_uri: "https://oauth2.googleapis.com/token",
-//   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-//   client_x509_cert_url:
-//     "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-6rktm%40node-quote-database.iam.gserviceaccount.com",
-//   client_email: "firebase-adminsdk-6rktm@node-quote-database.iam.gserviceaccount.com",
-//   private_key_id: `${process.env.FIREBASE_PRIVATE_KEY_ID}`,
-//   private_key: `${process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")}`,
-//   client_id: `${process.env.FIREBASE_CLIENT_ID}`,
-// };
-
 const config: ServiceAccount = {
   projectId: "node-quote-database",
   clientEmail: `${process.env.FIREBASE_CLIENT_EMAIL}`,
