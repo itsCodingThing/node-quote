d7766779c4b692507de4b7000fdc514476732bb0
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-05-30 12:05:16 +0530

    updating project

--- src/routers/homeRouter.ts
+++ src/routers/homeRouter.ts
@@ -7,32 +7,32 @@ import { QuoteObj } from "../utils/interfaces";
 const homeRouter = express.Router();
 
 homeRouter.get("/", (_req, res) => {
-  res.send(`you are looking for this /quote url`);
+    res.send(`you are looking for this /quote url`);
 });
 
 homeRouter.get("/quote", async (_req, res) => {
-  try {
-    const quote = await fetchQuote();
-    const savedQuote = await save(quote);
-    res.send(sendServerResponse({ ok: true, response: savedQuote }));
-  } catch {
-    res.send(
-      sendServerResponse({
-        ok: false,
-        error: "There must be some problem with backend",
-      }),
-    );
-  }
+    try {
+        const quote = await fetchQuote();
+        const savedQuote = await save(quote);
+        res.send(sendServerResponse({ ok: true, response: savedQuote }));
+    } catch {
+        res.send(
+            sendServerResponse({
+                ok: false,
+                error: "There must be some problem with backend",
+            }),
+        );
+    }
 });
 
 homeRouter.post("/add_quote", (req, res) => {
-  const { title = "", content = "" }: QuoteObj = req.body;
+    const { title = "", content = "" }: QuoteObj = req.body;
 
-  if (title && content) {
-    res.send(`${title}-${content}`);
-  } else {
-    res.status(500).send("please provide complete details");
-  }
+    if (title && content) {
+        res.send(`${title}-${content}`);
+    } else {
+        res.status(500).send("please provide complete details");
+    }
 });
 
 export default homeRouter;
